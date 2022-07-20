b6d7b4e58c9364eccafff4d876adcd84894e7015
Ritesh Hon <47925327+riteshhon@users.noreply.github.com>
2022-07-21 03:16:48 +0530

    Update code
    It is request to update your previous code to this, The code is neat and clean also easy to understand.

--- M3 Single & Double Dimensional Arrays/M3_T4_E3/m3_t4_e3.c
+++ M3 Single & Double Dimensional Arrays/M3_T4_E3/m3_t4_e3.c
@@ -1,38 +1,39 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
-/* following is the definition of the function transpose()
-that receives input array passed from the calling function */
-void transpose(int x[][3])
-{
-	int y[3][3],i,j;  // declaration of output array y and other variables
-	for (i=0;i<3;++i)   // outer loop for rows of x
-	{
-		for (j=0;j<3;++j)  // inner loop for columns of x
-{
-
-		y[i][j]=x[j][i];    // assigning transpose values into y
-} // end of inner loop
-	} // end of outer loop
-	// printing the output matrix 
-	for (i=0;i<3;++i)
-	{
-		for(j=0;j<3;++j)
-		{
-			printf("%d\t",y[i][j]);
-		}
-	printf("\n");  //newline feed as next row will be printed 
-	}
-	
+int main() {
+    // Initilizing custom array
+    int a[][3]={
+        {30,50,70},
+        {50,80,90},
+        {60,10,20}
+    };
+    
+    // calling transpose function
+    transposeMatrix(a);
 }
-// definition of the function main()
-int main()
-{
-/* following is the declaration and initialization of input matrix arr */
-	int arr[][3]={
-	{1,2,3},
-	{3,4,5},
-	{5,6,7}
-		} ;
-transpose(arr);  // calling the function by passing 2-d array arr	
-	
-}  //end of main()
+
+int transposeMatrix(int a[3][3]){
+    
+    // Declaring new array to store transpose array
+    int b[3][3];
+    
+    // Transposeing array of a and storing it in array b
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            b[i][j]=a[j][i];
+        }
+    }
+    
+    // Printing array transposion array b
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+}
